5548f5a4706a578fede25dea3514601565ec5774
j-chou <j-chou@users.noreply.github.com>
2015-04-09 12:55:10 -0500

    Update get_FN_rates.sh

--- src-pipeline/get_FN_rates.sh
+++ src-pipeline/get_FN_rates.sh
@@ -11,7 +11,7 @@ touch FN_svd_qmc_${n}_${k}
 #touch FN_svd_wqmc_${n}_${k}_${WCODE}
 cd ~/SVD_experiments/AGBsvdquartets/
                   
-for FOLDERNUMBER in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50; do 
+for FOLDERNUMBER in {1..50}; do 
     cd ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/${FOLDERNUMBER}
     cp ~/phylogenetic_tools/tree_comp/CompareTree.pl .
     cp ~/phylogenetic_tools/tree_comp/MOTree.pm .
