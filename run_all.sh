b26c4bf12c91fddfded86a1d936d84d96af360f6
agupta0905 <agupta0905@gmail.com>
2015-04-15 02:10:32 -0500

    to run pipeline for all parameters for a model condition

---
+++ run_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for t in $(seq 1 50)
+do
+	for n in 25 50 100
+	do
+		for k in 1 5 10 25
+		do
+			for wcode in A B C D E F G
+			do
+				echo "running for species="$t" for n="$n" for k="$k" for wcode="$wcode
+				./run_pipeline_cluster.sh data/model.10.1800000.0.000000111/$t scratch $n $k $wcode 0 > logs/output_$t"_"$n"_"$k"_"$wcode.txt
+			done
+		done
+	done
+done  
+
