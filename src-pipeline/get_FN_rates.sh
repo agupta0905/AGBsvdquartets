19e57834e21fd4072ec61a3357463ea6bea10955
Jed Chou <jedchou1@illinois.edu>
2015-04-15 08:43:18 -0500

    edit get_FN_rates

--- src-pipeline/get_FN_rates.sh
+++ src-pipeline/get_FN_rates.sh
@@ -1,15 +1,16 @@
 #!/bin/bash                                                                      
 
-n=$1            
-k=$2
-WCODE=$3
+n=$1   
  
-cd ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}
-rm -f FN_svd_qmc_${n}_${k}
-#rm -f FN_svd_wqmc_${n}_${k}_${WCODE}
-touch FN_svd_qmc_${n}_${k}
-#touch FN_svd_wqmc_${n}_${k}_${WCODE}
-cd ~/SVD_experiments/AGBsvdquartets/
+cd ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111
+for k in 1 5 10 25; do
+    rm -f FN_svd_qmc_${n}_${k}
+    touch FN_svd_qmc_${n}_${k}
+    for WCODE in A B C D E F G; do
+	rm -f FN_svd_wqmc_${n}_${k}_${WCODE}
+	touch FN_svd_wqmc_${n}_${k}_${WCODE}
+    done
+done
                   
 for FOLDERNUMBER in {1..50}; do 
     cd ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/${FOLDERNUMBER}
@@ -17,13 +18,16 @@ for FOLDERNUMBER in {1..50}; do
     cp ~/phylogenetic_tools/tree_comp/MOTree.pm .
     cp ~/phylogenetic_tools/tree_comp/compareTrees .
     cp ~/phylogenetic_tools/tree_comp/compareTrees.missingBranch .
-    ./compareTrees.missingBranch S_relabeled_tree.trees svd_qmc_s_tree_${n}_${k}.trees >> ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}/FN_svd_qmc_${n}_${k}
-	
-    #./compareTrees.missingBranch S_relabeled_tree.trees svd_wqmc_s_tree_${n}_${k}_${WCODE}.trees >> ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}/FN_svd_wqmc_${n}_${k}_${WCODE}
+    for k in 1 5 10 25; do
+	./compareTrees.missingBranch S_relabeled_tree.trees svd_qmc_s_tree_${n}_${k}.trees >> ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/FN_svd_qmc_${n}_${k}
+	for WCODE in A B C D E F G; do
+	    ./compareTrees.missingBranch S_relabeled_tree.trees svd_wqmc_s_tree_${n}_${k}_${WCODE}.trees >> ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/FN_svd_wqmc_${n}_${k}_${WCODE}
+	done
+    done
     rm CompareTree.pl
     rm MOTree.pm
     rm compareTrees*
 done
 
-awk -vORS=, '{ print $3 }' ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}/FN_svd_qmc_${n}_${k} | sed 's/,$/\n/'
+#awk -vORS=, '{ print $3 }' ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}/FN_svd_qmc_${n}_${k} | sed 's/,$/\n/'
 #awk -vORS=, '{ print $3 }' ~/SVD_experiments/AGBsvdquartets/data/model.10.1800000.0.000000111/results_${n}_${k}/FN_svd_wqmc_${n}_${k}_${WCODE} | sed 's/,$/\n/'
