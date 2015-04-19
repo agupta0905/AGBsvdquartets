71d5144d73f6b3ecd2df69a2145d2627f31576ad
agupta0905 <agupta0905@gmail.com>
2015-04-18 22:52:13 -0500

    temporary wqmc and qmc trees stored

--- run_pipeline_cluster.sh
+++ run_pipeline_cluster.sh
@@ -15,7 +15,6 @@ if [  ! -d ${SPECIES_PATH} ]; then
 	echo "CREATING DIRECTORY ${SPECIES_PATH}"
 	mkdir -p ${SPECIES_PATH}
 fi
-
 if [  ! -d "${DATA_PARENT}/${SPECIES_PATH}/relabeled_data" ]; then
 	echo "RELABELING DATA AT ${SPECIES_PATH}"
 	python src-pipeline/taxon_relabeler.py "${DATA_PARENT}/${SPECIES_PATH}" data/taxa_dict.txt
@@ -38,7 +37,6 @@ if [  ! -d "${DATA_PARENT}/${SPECIES_PATH}/relabeled_shortened_sampled_data_${N}
 	echo "SAMPLING GENES FOR ${SPECIES_PATH} for N=${N} for k=${K}"
 	python src-pipeline/gene_sampler.py "${DATA_PARENT}/${SPECIES_PATH}" ${N} ${K}
 fi
-
 if [  ! -e "${SPECIES_PATH}/relabeled_shortened_sampled_combined_${N}_${K}.nex" ]; then
 	echo "AGGREGATING SAMPLED GENES FOR ${SPECIES_PATH} for N=${N} for k=${K}"
 	python src-pipeline/gene_aggregator.py "${DATA_PARENT}/${SPECIES_PATH}" ${N} ${K}
@@ -61,7 +59,8 @@ rm -f qmc_quartet_trees.txt
 rm -f wqmc_quartet_trees.txt
 echo "CONVERTING SVDQUARTET OUTPUT TO QMC/WQMC using WFCode ${WCode}"
 python "src-pipeline/convert_svd_qmc_wqmc_${WCode}.py"
-
+cp qmc_quartet_trees.txt "${SPECIES_PATH}/qmc_quartet_trees_${N}_${K}_${WCode}.txt"
+cp wqmc_quartet_trees.txt "${SPECIES_PATH}/wqmc_quartet_trees_${N}_${K}_${WCode}.txt"
 echo "RUNNING QMC"
 ./src-pipeline/max-cut-tree qrtt=qmc_quartet_trees.txt weights=off otre="${SPECIES_PATH}/svd_qmc_s_tree_${N}_${K}.trees"
 
