da46cae15e9db39fffa7dc30838e756bfd1d9c91
agupta0905 <agupta0905@gmail.com>
2015-04-18 22:53:43 -0500

    added running for new weight functions

--- run_all.sh
+++ run_all.sh
@@ -5,10 +5,10 @@ do
 	do
 		for k in 1 5 10 25
 		do
-			for wcode in A B C D E F G
+			for wcode in A B C D E F G H I J K L
 			do
 				echo "running for species="$t" for n="$n" for k="$k" for wcode="$wcode
-				./run_pipeline_cluster.sh data/model.10.1800000.0.000000111/$t scratch $n $k $wcode 0 > logs/output_$t"_"$n"_"$k"_"$wcode.txt
+				./run_pipeline_cluster.sh data/model.10.5400000.0.000000037/$t scratch $n $k $wcode 0 > logs/output_$t"_"$n"_"$k"_"$wcode.txt
 			done
 		done
 	done
